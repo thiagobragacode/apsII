85a4f38b35ba5199d8c9e77213ebe428287d05d0
Thiago Braga <thiagoth.dantas08@gmail.com>
2025-09-18 19:41:11 -0300

    Create Fita.java

---
+++ Fita.java
@@ -0,0 +1,25 @@
+public class Fita {
+    public static final int NORMAL = 0;
+    public static final int LANCAMENTO = 1;
+    public static final int INFANTIL = 2;
+    
+    private String titulo;
+    private int codigoDePreco;
+    
+    public Fita(String titulo, int codigoDePreco) {
+        this.titulo = titulo;
+        this.codigoDePreco = codigoDePreco;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public int getCodigoDePreco() {
+        return codigoDePreco;
+    }
+    
+    public void setCodigoDePreco(int codPreco) {
+        this.codigoDePreco = codPreco;
+    }
+}
